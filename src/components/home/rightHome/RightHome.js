4657f470c3c15cb9b157daca53580c1c45e5aa0e
Deia Hamad <engdeiaaeldin@gmail.com>
2022-07-07 08:31:45 +0400

    deploying my portfolio

--- src/components/home/rightHome/RightHome.js
+++ src/components/home/rightHome/RightHome.js
@@ -6,7 +6,7 @@ function RightHome() {
       <h1>
         I'm <span>Deia</span>.
       </h1>
-      <h3>A Front-End Web Designer.</h3>
+      <h3>A Web Designer.</h3>
       <p>
         Hi, I'm a web designer based in Abu Dhabi, UAE. <br />I have a passion
         for web design and love to create for web and mobile devices.
