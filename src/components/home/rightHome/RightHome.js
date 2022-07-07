68bc913a0270ca4517f103a2f5084a3acb39d2df
DeiaHamad <100486718+DeiaHamad@users.noreply.github.com>
2022-07-07 09:04:39 +0400

    Update RightHome.js
    changing some data

--- src/components/home/rightHome/RightHome.js
+++ src/components/home/rightHome/RightHome.js
@@ -6,9 +6,9 @@ function RightHome() {
       <h1>
         I'm <span>Deia</span>.
       </h1>
-      <h3>A Web Designer.</h3>
+      <h3>A Front-End Web Developer.</h3>
       <p>
-        Hi, I'm a web desinger based in Abu Dhabi, UAE. I have a passion for
+        Hi, I'm a web developer based in Abu Dhabi, UAE. I have a passion for
         web design and love to create for web and mobile devices.
       </p>
     </article>
