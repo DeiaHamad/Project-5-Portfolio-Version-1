8ebbf1f9c482a1e8da674d02ba71f95eb94d8a83
Deia Hamad <engdeiaaeldin@gmail.com>
2022-07-15 11:05:11 +0400

    changing some data

--- src/data/myData.js
+++ src/data/myData.js
@@ -1,20 +1,20 @@
-import React from "react";
-import { BiBookReader } from "react-icons/bi";
-import { FiUsers } from "react-icons/fi";
-import { IoMdCodeWorking } from "react-icons/io";
-import { IoLogoHtml5 } from "react-icons/io";
-import { IoLogoCss3 } from "react-icons/io";
-import { SiJavascript } from "react-icons/si";
-import { FaReact } from "react-icons/fa";
-import { DiSass } from "react-icons/di";
-import { SiTestinglibrary } from "react-icons/si";
-import { GoLocation } from "react-icons/go";
-import { MdSchool } from "react-icons/md";
-import { MdOutlineLanguage } from "react-icons/md";
-import { MdEmail } from "react-icons/md";
-import { BsPhoneFill } from "react-icons/bs";
-import { AiFillLinkedin } from "react-icons/ai";
-import { AiFillGithub } from "react-icons/ai";
+import React from 'react';
+import { BiBookReader } from 'react-icons/bi';
+import { FiUsers } from 'react-icons/fi';
+import { IoMdCodeWorking } from 'react-icons/io';
+import { IoLogoHtml5 } from 'react-icons/io';
+import { IoLogoCss3 } from 'react-icons/io';
+import { SiJavascript } from 'react-icons/si';
+import { FaReact } from 'react-icons/fa';
+import { DiSass } from 'react-icons/di';
+import { SiTestinglibrary } from 'react-icons/si';
+import { GoLocation } from 'react-icons/go';
+import { MdSchool } from 'react-icons/md';
+import { MdOutlineLanguage } from 'react-icons/md';
+import { MdEmail } from 'react-icons/md';
+import { BsPhoneFill } from 'react-icons/bs';
+import { AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub } from 'react-icons/ai';
 
 const [num, unit] = handleExperience();
 
@@ -30,38 +30,38 @@ const myData = {
   },
   skills: [
     {
-      name: "Html 5",
-      lvl: "Intermediate",
+      name: 'Html 5',
+      lvl: 'Intermediate',
       percentage: 75,
       icon: <IoLogoHtml5 className="info-icon" data-testid="skill-icon" />,
     },
     {
-      name: "Css 3",
-      lvl: "Intermediate",
+      name: 'Css 3',
+      lvl: 'Intermediate',
       percentage: 70,
       icon: <IoLogoCss3 className="info-icon" data-testid="skill-icon" />,
     },
     {
-      name: "Sass",
-      lvl: "Beginner",
+      name: 'Sass',
+      lvl: 'Beginner',
       percentage: 65,
       icon: <DiSass className="info-icon" data-testid="skill-icon" />,
     },
     {
-      name: "Javascript",
-      lvl: "Intermediate",
+      name: 'Javascript',
+      lvl: 'Intermediate',
       percentage: 75,
       icon: <SiJavascript className="info-icon" data-testid="skill-icon" />,
     },
     {
-      name: "React",
-      lvl: "Intermediate",
+      name: 'React',
+      lvl: 'Intermediate',
       percentage: 70,
       icon: <FaReact className="info-icon" data-testid="skill-icon" />,
     },
     {
-      name: "React Testing Library",
-      lvl: "Beginner",
+      name: 'React Testing Library',
+      lvl: 'Beginner',
       percentage: 65,
       icon: <SiTestinglibrary className="info-icon" data-testid="skill-icon" />,
     },
@@ -70,248 +70,248 @@ const myData = {
     icon: <IoMdCodeWorking className="info-icon" />,
     array: [
       {
-        id: "p1",
-        title: "Blackjack",
-        liveDeom: "https://deiahamad.github.io/Project-1-BlackJack/",
-        github: "https://github.com/DeiaHamad/Project-1-BlackJack",
+        id: 'p1',
+        title: 'Blackjack',
+        liveDeom: 'https://deiahamad.github.io/Project-1-BlackJack/',
+        github: 'https://github.com/DeiaHamad/Project-1-BlackJack',
         technology: [
           {
-            lang: "Html",
+            lang: 'Html',
             icon: <IoLogoHtml5 className="project-icon" />,
             codeLines: 92,
           },
           {
-            lang: "Css",
+            lang: 'Css',
             icon: <IoLogoCss3 className="project-icon" />,
             codeLines: 261,
           },
           {
-            lang: "Javascript",
+            lang: 'Javascript',
             icon: <SiJavascript className="project-icon" />,
             codeLines: 507,
           },
         ],
-        startingDate: "Mar 27, 2022",
-        finishingDate: "Apr 2, 2022",
-        duration: handleProjects("Mar 27, 2022", "Apr 2, 2022"),
+        startingDate: 'Mar 27, 2022',
+        finishingDate: 'Apr 2, 2022',
+        duration: handleProjects('Mar 27, 2022', 'Apr 2, 2022'),
       },
       {
-        id: "p2",
-        title: "Chrome Extension",
+        id: 'p2',
+        title: 'Chrome Extension',
         github:
-          "https://github.com/DeiaHamad/Project-2-Chrome-Save-Url-Extension",
+          'https://github.com/DeiaHamad/Project-2-Chrome-Save-Url-Extension',
         technology: [
           {
-            lang: "Html",
+            lang: 'Html',
             icon: <IoLogoHtml5 className="project-icon" />,
             codeLines: 21,
           },
           {
-            lang: "Css",
+            lang: 'Css',
             icon: <IoLogoCss3 className="project-icon" />,
             codeLines: 68,
           },
           {
-            lang: "Javascript",
+            lang: 'Javascript',
             icon: <SiJavascript className="project-icon" />,
             codeLines: 80,
           },
         ],
-        startingDate: "Apr 2, 2022",
-        finishingDate: "Apr 3, 2022",
-        duration: handleProjects("Apr 2, 2022", "Apr 3, 2022"),
+        startingDate: 'Apr 2, 2022',
+        finishingDate: 'Apr 3, 2022',
+        duration: handleProjects('Apr 2, 2022', 'Apr 3, 2022'),
       },
       {
-        id: "p3",
-        title: "CourseTube-V1",
-        liveDeom: "https://deiahamad.github.io/Project-3-CourseTube/",
-        github: "https://github.com/DeiaHamad/Project-3-CourseTube",
+        id: 'p3',
+        title: 'CourseTube-V1',
+        liveDeom: 'https://deiahamad.github.io/Project-3-CourseTube-Version-1/',
+        github: 'https://github.com/DeiaHamad/Project-3-CourseTube-Version-1',
         technology: [
           {
-            lang: "Html",
+            lang: 'Html',
             icon: <IoLogoHtml5 className="project-icon" />,
             codeLines: 196,
           },
           {
-            lang: "Css",
+            lang: 'Css',
             icon: <IoLogoCss3 className="project-icon" />,
             codeLines: 780,
           },
           {
-            lang: "Javascript",
+            lang: 'Javascript',
             icon: <SiJavascript className="project-icon" />,
             codeLines: 604,
           },
         ],
-        startingDate: "Apr 9, 2022",
-        finishingDate: "Apr 16, 2022",
-        duration: handleProjects("Apr 9, 2022", "Apr 16, 2022"),
+        startingDate: 'Apr 9, 2022',
+        finishingDate: 'Apr 16, 2022',
+        duration: handleProjects('Apr 9, 2022', 'Apr 16, 2022'),
       },
       {
-        id: "p4",
-        title: "CourseTube-V2",
-        liveDeom: "https://deiahamad.github.io/Project-4-CourseTube-Version-2/",
-        github: "https://github.com/DeiaHamad/Project-4-CourseTube-Version-2",
+        id: 'p4',
+        title: 'CourseTube-V2',
+        liveDeom: 'https://deiahamad.github.io/Project-4-CourseTube-Version-2/',
+        github: 'https://github.com/DeiaHamad/Project-4-CourseTube-Version-2',
         technology: [
           {
-            lang: "Html",
+            lang: 'Html',
             icon: <IoLogoHtml5 className="project-icon" />,
             codeLines: 20,
           },
           {
-            lang: "Sass",
+            lang: 'Sass',
             icon: <DiSass className="project-icon" />,
             codeLines: 1134,
           },
           {
-            lang: "React",
+            lang: 'React',
             icon: <FaReact className="project-icon" />,
             codeLines: 2678,
           },
           {
-            lang: "Test",
+            lang: 'Test',
             icon: <SiTestinglibrary className="project-icon" />,
             codeLines: 3304,
           },
         ],
-        startingDate: "May 5, 2022",
-        finishingDate: "June 19, 2022",
-        duration: handleProjects("May 5, 2022", "June 19, 2022"),
+        startingDate: 'May 5, 2022',
+        finishingDate: 'June 19, 2022',
+        duration: handleProjects('May 5, 2022', 'June 19, 2022'),
       },
       {
-        id: "p5",
-        title: "Portfolio",
-        github: "https://github.com/DeiaHamad/Project-5-Portfolio",
+        id: 'p5',
+        title: 'Portfolio',
+        github: 'https://github.com/DeiaHamad/Project-5-Portfolio-Version-1/',
         technology: [
           {
-            lang: "Html",
+            lang: 'Html',
             icon: <IoLogoHtml5 className="project-icon" />,
             codeLines: 16,
           },
           {
-            lang: "Sass",
+            lang: 'Sass',
             icon: <DiSass className="project-icon" />,
             codeLines: 1205,
           },
           {
-            lang: "React",
+            lang: 'React',
             icon: <FaReact className="project-icon" />,
             codeLines: 1257,
           },
           {
-            lang: "Test",
+            lang: 'Test',
             icon: <SiTestinglibrary className="project-icon" />,
             codeLines: 1076,
           },
         ],
-        startingDate: "June 28, 2022",
-        finishingDate: "July 5, 2022",
-        duration: handleProjects("June 28, 2022", "July 5, 2022"),
+        startingDate: 'June 28, 2022',
+        finishingDate: 'July 5, 2022',
+        duration: handleProjects('June 28, 2022', 'July 5, 2022'),
       },
     ],
   },
   jobs: [
     {
-      id: "j1",
-      jobTitle: "Sales",
-      company: "Mg Group",
-      startDate: "Oct / 2012",
-      endDate: "Dec / 2012",
-      description: "Tele-Sales & Outdoor Sales.",
-      country: "Egypt",
+      id: 'j1',
+      jobTitle: 'Sales',
+      company: 'Mg Group',
+      startDate: 'Oct / 2012',
+      endDate: 'Dec / 2012',
+      description: 'Tele-Sales & Outdoor Sales.',
+      country: 'Egypt',
     },
     {
-      id: "j2",
-      jobTitle: "Technical Support",
-      company: "TE Data",
-      startDate: "2013",
-      endDate: "2016",
-      description: "Internet technical support by phone.",
-      country: "Egypt",
+      id: 'j2',
+      jobTitle: 'Technical Support',
+      company: 'TE Data',
+      startDate: '2013',
+      endDate: '2016',
+      description: 'Internet technical support by phone.',
+      country: 'Egypt',
     },
     {
-      id: "j3",
-      jobTitle: "Customer Service (Enterprise)",
-      company: "Vodafone UK",
-      startDate: "Dec / 2016",
-      endDate: "Jan / 2017",
-      description: "Internet technical support by phone (British Customers).",
-      country: "Egypt",
+      id: 'j3',
+      jobTitle: 'Customer Service (Enterprise)',
+      company: 'Vodafone UK',
+      startDate: 'Dec / 2016',
+      endDate: 'Jan / 2017',
+      description: 'Internet technical support by phone (British Customers).',
+      country: 'Egypt',
     },
     {
-      id: "j4",
-      jobTitle: "Foreman",
-      company: "Nazih Blacksmith & Welding Workshop",
-      startDate: "2017",
-      endDate: "2018",
+      id: 'j4',
+      jobTitle: 'Foreman',
+      company: 'Nazih Blacksmith & Welding Workshop',
+      startDate: '2017',
+      endDate: '2018',
       description:
-        "Managing the work and ensuring delivery on time with good quality.",
-      country: "UAE",
+        'Managing the work and ensuring delivery on time with good quality.',
+      country: 'UAE',
     },
     {
-      id: "j5",
-      jobTitle: "Safety Engineer",
-      company: "ICAD Residential City",
-      startDate: "2018",
-      endDate: "2019",
-      description: "Handling the safety issues regarding the workers/shops.",
-      country: "UAE",
+      id: 'j5',
+      jobTitle: 'Safety Engineer',
+      company: 'ICAD Residential City',
+      startDate: '2018',
+      endDate: '2019',
+      description: 'Handling the safety issues regarding the workers/shops.',
+      country: 'UAE',
     },
     {
-      id: "j6",
-      jobTitle: "Designer",
-      company: "Nazih Blacksmith & Welding Workshop",
-      startDate: "2019",
-      endDate: "now",
+      id: 'j6',
+      jobTitle: 'Designer',
+      company: 'Nazih Blacksmith & Welding Workshop',
+      startDate: '2019',
+      endDate: 'now',
       description:
-        "Provide an Autocad designs (2D & 3D) and manging the work regarding the quality and time.",
-      country: "UAE",
+        'Provide an Autocad designs (2D & 3D) and manging the work regarding the quality and time.',
+      country: 'UAE',
     },
   ],
   contacts: {
     details: [
       {
-        id: "location",
+        id: 'location',
         icon: <GoLocation className="contact-icon" />,
-        title: "Location:",
-        text: "Abu Dhabi - United Arab Emirates",
+        title: 'Location:',
+        text: 'Abu Dhabi - United Arab Emirates',
       },
       {
-        id: "education",
+        id: 'education',
         icon: <MdSchool className="contact-icon" />,
-        title: "Education:",
-        text: "Institute of Aviation Engineering & Technology - Egypt",
+        title: 'Education:',
+        text: 'Institute of Aviation Engineering & Technology - Egypt',
       },
       {
-        id: "language",
+        id: 'language',
         icon: <MdOutlineLanguage className="contact-icon" />,
-        title: "Language:",
-        text: "English, Arabic",
+        title: 'Language:',
+        text: 'English, Arabic',
       },
       {
-        id: "email",
+        id: 'email',
         icon: <MdEmail className="contact-icon" />,
-        title: "Email:",
-        text: "EngDeiaaEldin@gmail.com",
+        title: 'Email:',
+        text: 'EngDeiaaEldin@gmail.com',
       },
       {
-        id: "phoneNumber",
+        id: 'phoneNumber',
         icon: <BsPhoneFill className="contact-icon" />,
-        title: "Phone Number:",
-        text: "+97150 789 0072",
+        title: 'Phone Number:',
+        text: '+97150 789 0072',
       },
     ],
     socialMedia: [
       {
-        id: "linkedin",
-        link: "https://www.linkedin.com/in/deia-hamad-577a1814a/",
+        id: 'linkedin',
+        link: 'https://www.linkedin.com/in/deia-hamad-577a1814a/',
         icon: (
           <AiFillLinkedin className="media-icon" data-testid="linkedin-icon" />
         ),
       },
       {
-        id: "github",
-        link: "https://www.github.com/DeiaHamad",
+        id: 'github',
+        link: 'https://www.github.com/DeiaHamad',
         icon: <AiFillGithub className="media-icon" data-testid="github-icon" />,
       },
     ],
@@ -328,7 +328,7 @@ function handleExperience() {
     return date;
   }
 
-  const startingDate = convertDateForIos("2021-11-1 00:00:00").getTime();
+  const startingDate = convertDateForIos('2021-11-1 00:00:00').getTime();
   const currentDate = new Date().getTime();
 
   const expInMonths = Math.floor(
@@ -339,13 +339,13 @@ function handleExperience() {
     const expInYears = expInMonths / 12;
     num = Math.ceil(expInYears);
     if (expInYears < 2) {
-      unit = "Year";
+      unit = 'Year';
     } else {
-      unit = "Years";
+      unit = 'Years';
     }
   } else {
     num = expInMonths;
-    unit = "Months";
+    unit = 'Months';
   }
 
   return [num, unit];
